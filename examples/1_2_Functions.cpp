da292cbf07abb621abda48210d257a67de1dc920
Wiktor Włodarski <wiktorwlodarski@gmail.com>
2019-02-27 12:52:33 +0100

    Second example, functions.

---
+++ examples/1_2_Functions.cpp
@@ -0,0 +1,15 @@
+// Functions - przyk³ad w³asnych funkcji
+
+#include <iostream>
+#include <conio.h>
+
+void ShowText()
+{
+	std::cout << "Sample function" << std::endl;
+}
+
+void main()
+{
+	ShowText();
+	_getch();
+}
