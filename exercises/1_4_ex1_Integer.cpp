630fb757f03df3f486a79afa55cddcbeed7d34af
Wiktor Włodarski <wiktorwlodarski@gmail.com>
2019-03-04 15:24:43 +0100

    Exercise to demonstrate intiger overflow.

---
+++ exercises/1_4_ex1_Integer.cpp
@@ -0,0 +1,15 @@
+// Integer - program wyœwietlaj¹cy liczbê zapisan¹ dwoma integerami.
+
+#include <iostream>
+#include <conio.h>
+
+void main()
+{
+	int nMiliard = 3000000000;
+	unsigned int nMiliard2 = 3000000000;
+
+	std::cout << "int = " << nMiliard << std::endl;
+	std::cout << "unsigned int = " << nMiliard2 << std::endl;
+
+	_getch();
+}
