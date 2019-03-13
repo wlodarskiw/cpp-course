d778a16d85981d9a96dd7bb5956887690fd29b46
Wiktor Włodarski <wiktorwlodarski@gmail.com>
2019-03-13 14:08:51 +0100

    Modification to TicTacToe from example to prevent user inputing letters instead of field numbers.

---
+++ exercises/1_5_ex3&4_TicTacToe2/game.h
@@ -0,0 +1,42 @@
+// Plik nag³ówkowy w³aœciwego kodu gry
+
+// zabezpieczenie przez wielokrotnym do³¹czeniem
+#pragma once
+
+// typy
+//------------------------------------------------------------------------------
+
+// znak - czyli kó³ko lub krzy¿yk
+enum SIGN { SGN_CIRCLE = 'O', SGN_CROSS = 'X' };
+
+// pojedyncze pole (mo¿e byæ puste, mieæ kó³ko lub krzy¿yk)
+enum FIELD { FLD_EMPTY = 0,				// puste
+			 FLD_CIRCLE = SGN_CIRCLE,	// kó³ko
+			 FLD_CROSS = SGN_CROSS };	// krzy¿yk
+
+// stan gry
+enum GAMESTATE { GS_NOTSTARTED,	// nie rozpoczêta
+				 GS_MOVE,		// ruch gracza
+				 GS_WON,		// wygrana gracza
+				 GS_DRAW };		// remis
+
+// deklaracje zmiennych
+//------------------------------------------------------------------------------
+
+// zmienna okreœlaj¹ca stan gry musi byæ widoczna tak¿e na zewn¹trz
+// modu³u game.cpp; poni¿sza deklaracja czyni zadoœæ tej potrzebie
+
+// stan gry
+extern GAMESTATE g_StanGry;
+
+// prototypy funkcji
+//------------------------------------------------------------------------------
+
+// rozpoczêcie gry
+bool StartGry();
+
+// narysowanie planszy
+bool RysujPlansze();
+
+// wykonanie ruchu
+bool Ruch(unsigned);
---
+++ exercises/1_5_ex3&4_TicTacToe2/main.cpp
@@ -0,0 +1,38 @@
+// TicTacToe - wielki projekt gry typu "kó³ko i krzy¿yk"
+
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include "game.h"
+
+void main()
+{
+	// rozpoczynamy grê
+	StartGry();
+
+	// pêtla nieskoñczona - czêsty element w grach
+	// wyjdziemy z niej przy pomocy break w odpowiednim momencie
+	for (;;)
+	{
+		// rysujemy planszê
+		RysujPlansze();
+
+		// sprawdzamy stan gry i czynimy odpowiednie dzia³ania
+		if (g_StanGry == GS_MOVE)
+		{
+			// ruch któregoœ gracza
+			// pobieramy go wiêc i wywo³ujemy funkcjê Ruch()
+			// pobieramy znak który wstawi u¿ytkownik i castujemy go na unsigned
+			char chNumerPola;
+			std::cin >> chNumerPola;
+			Ruch(static_cast<unsigned>(chNumerPola));
+		}
+		else if (g_StanGry == GS_WON || g_StanGry == GS_DRAW)
+			// koniec gry (wygran¹ lub remisem)
+			// wtedy przerywamy pêtlê
+			break;
+	}
+
+	// czekamy na dowolny klawisz
+	_getch();
+}
